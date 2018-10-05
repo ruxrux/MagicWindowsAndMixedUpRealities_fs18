942f616c4834023ccf3b7042f9eebe0ab7bb9cc9
rux <rux@ruxs-MBP.home>
2018-10-04 22:16:07 -0400

    class 4 example added

---
+++ WEEK04/c4_LittleYellowBlue/Assets/_scripts/beGreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beGreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	private void OnCollisionEnter(Collision col)
+	{
+        Debug.Log("collided with : " + col.gameObject.name);
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Standard");
+        rend.material.SetColor("_Color", Color.green);
+
+        if (col.gameObject.name == "yellow"){
+            // do something!
+        }
+	}
+}
