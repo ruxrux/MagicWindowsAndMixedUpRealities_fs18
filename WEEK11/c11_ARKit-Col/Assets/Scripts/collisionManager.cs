de8142887fd4f6401adcc249957c6d5218d39903
rux <rux@ruxs-MBP.home>
2018-11-15 00:35:32 -0500

    c11 example added
    collisions

---
+++ WEEK11/c11_ARKit-Col/Assets/Scripts/collisionManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionManager : MonoBehaviour {
+    AudioSource sample;
+	// Use this for initialization
+	void Start () {
+        sample = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+        Debug.Log("col with " + collision.collider.name);
+        sample.Play();
+	}
+}
---
+++ WEEK11/c11_ARKit-Col/Assets/Scripts/shoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+
+    public GameObject ball; //reference to the ball prefab, set in editor    
+    private GameObject ballClone; //we don't use the original prefab
+    private AudioSource sample;
+
+   	void Start () {
+        sample = GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Shoot();
+            }
+        }
+
+        // lets delete the ball is it's close to the ground
+        if (ballClone != null && ballClone.transform.position.y < -5.0f)
+        {
+            Destroy(ballClone);
+        }
+	}
+
+    void Shoot (){
+
+        ballClone = Instantiate(ball, transform.position, transform.rotation);
+        ballClone.GetComponent<Rigidbody>().velocity = transform.forward * 5.0f;
+        sample.Play();
+
+    }
+}
